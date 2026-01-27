9c81ba3a24189d9a4b35ef38be6f62448773d330
Agustin Escobar <agusesgi@gmail.com>
2026-01-27 18:54:33 -0300

    Crear proyecto con librerías básicas

---
+++ src/components/navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+
+function NavBar() {
+  return (
+    <nav className="flex justify-between">
+      <h1>Prueba</h1>
+      <div className="flex gap-x-2 items-center">
+        <Link href="/" className={buttonVariants({ variant: "secondary" })}>
+          Crear Empleado
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
